daa482591e78bc9fb30533c40650a32e6dd28be5
Owen Barton <owen@owenbarton.com>
2026-02-27 12:56:09 -0800

    Add refined httrack command.

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 # This documents the commands used to download the archive
 cd /data/source
-httrack 'https://opensourcevista.net/NancysVistAServer/' 'https://www.va.gov/vdl/' 'https://www.ihs.gov/rpms/' 'http://worldvista.org' 'https://hardhats.org' 'https://www.va.gov/vdl/documents/Monograph/Monograph/vista_monograph_0723_r.docx' 'http://osehra.s3-website-us-east-1.amazonaws.com/?prefix=tech_journal_archive/'
+httrack -c 3 -T 90 -R 9 '-*worldvista.org/*/view' '-*worldvista.org/*/download' 'https://opensourcevista.net/NancysVistAServer/' 'https://www.va.gov/vdl/' 'https://www.ihs.gov/rpms/' 'http://worldvista.org' 'https://hardhats.org' 'https://www.va.gov/vdl/documents/Monograph/Monograph/vista_monograph_0723_r.docx' 'http://osehra.s3-website-us-east-1.amazonaws.com/?prefix=tech_journal_archive/'
 wget2 --mirror --page-requisites --no-parent --robots=off --wait=1 --random-wait --reject-regex '\?C=|\?O=' https://code.worldvista.org/
 wget2 --mirror --page-requisites --no-parent --robots=off --wait=1 --random-wait --reject-regex '\?C=|\?O=' https://foia-vista.worldvista.org
 ORG=WorldVistA
