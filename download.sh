82fed063a29e1903e32366bfcd72fdf7914f975a
Owen Barton <owen@owenbarton.com>
2026-03-20 17:00:25 -0700

    Add a few missing URLs, replace S3 journal with WorldVistA archive.

--- download.sh
+++ download.sh
@@ -2,7 +2,18 @@
 
 # This documents the commands used to download the archive
 cd /data/source
-httrack -c 3 -T 90 -R 9 '-*worldvista.org/*/view' '-*worldvista.org/*/download' 'https://opensourcevista.net/NancysVistAServer/' 'https://www.va.gov/vdl/' 'https://www.ihs.gov/rpms/' 'http://worldvista.org' 'https://hardhats.org' 'https://www.va.gov/vdl/documents/Monograph/Monograph/vista_monograph_0723_r.docx' 'http://osehra.s3-website-us-east-1.amazonaws.com/?prefix=tech_journal_archive/'
+httrack --update -c 3 -T 90 -R 9 \
+'-*worldvista.org/*/view' '-*worldvista.org/*/download' \
+'https://opensourcevista.net/NancysVistAServer/' \
+'https://www.va.gov/vdl/' \
+'https://www.ihs.gov/rpms/' \
+'https://www.ihs.gov/sites/RPMS/SRCB/' \
+'http://worldvista.org' \
+'https://hardhats.org' \
+'https://www.va.gov/vdl/documents/Monograph/Monograph/vista_monograph_0723_r.docx' \
+'https://vistapedia.com/' \
+'https://journal.worldvista.org/' \
+'https://education.worldvista.org/'
 wget2 --mirror --page-requisites --no-parent --robots=off --wait=1 --random-wait --reject-regex '\?C=|\?O=' https://code.worldvista.org/
 wget2 --mirror --page-requisites --no-parent --robots=off --wait=1 --random-wait --reject-regex '\?C=|\?O=' https://foia-vista.worldvista.org
 ORG=WorldVistA
